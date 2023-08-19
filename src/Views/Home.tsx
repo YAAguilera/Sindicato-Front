6a50bb02e780ddb3a9129e5ab71bdd8aae5266c9
yose <yoseli21603@gmail.com>
2023-08-19 12:32:41 -0300

    carpetas y archivos

---
+++ src/Views/Home.tsx
@@ -0,0 +1,13 @@
+//import React from 'react'
+
+const Home = () => {
+  return (
+    <main>
+        <section className='overflow-x-hidden bg-darkBlue w-screen h-screen'>
+      
+        </section>
+    </main>
+  )
+}
+
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue:'#274C77',
+        lightBlue: '#6096BA',
+        lightGray: '#C5C6C4',
+        darkGray: '#8B8C89'
+      },
+      screens: {
+        xs: "0px", //0 a 639
+        sm: "640px", // 640 a 767
+        md: "768px", //768 a 1023
+        lg: "1024px", //1024 a 1279
+        xl: "1280px", //1280 a 1882
+        xxl: "1883px", 
+      },
+    },
   },
   plugins: [],
 }
